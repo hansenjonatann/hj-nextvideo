ac1235407db739676077a6e511f8794c3ec8edaa
hansenjonatann <hansenjonatann@gmail.com>
2025-07-08 09:05:28 +0700

    feat: add button to navigate to upload page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from "next/link"
 import { useEffect, useState } from "react"
 
 
@@ -22,12 +23,18 @@ export default  function VideosPage() {
 
   return (
     <div className="space-y-8 p-4">
-      {videos.map((v : any) => (
+ <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+       {videos.map((v : any) => (
         <div key={v.id}>
           <h2 className="font-bold text-xl">{v.title}</h2>
-          <video src={v.url} controls className="w-full h-auto" />
+          <video src={v.url} controls className="w-full mt-8 h-auto" />
         </div>
       ))}
+ </div>
+
+      <div className="fixed bottom-4 right-3 bg-blue-600 w-12 h-12 flex justify-center items-center rounded-full">
+        <Link href={'/upload'} className="text-3xl">+</Link>
+      </div>
     </div>
   )
 }
